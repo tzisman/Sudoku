4415c51ea3f500153578e8bbbadd12254e16a8e1
tzisman <t0527105154@gmail.com>
2026-06-21 12:49:55 +0300

    Initial ASP.NET Core project setup with solution and configuration

---
+++ Sudoku.Server/Program.cs
@@ -0,0 +1,25 @@
+using Sudoku.Server.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<SudokuService>();
+builder.Services.AddSingleton<PdfService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
